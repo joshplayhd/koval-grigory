72ddcdcbde912bfe39cbc92cb6f1b77d12cc47e3
grigory <joshplayhd@mail.ru>
2024-04-04 16:51:48 +0700

    Testing commit in VS Code

--- project_1.srcs/sim_1/new/tb_max.sv
+++ project_1.srcs/sim_1/new/tb_max.sv
@@ -1,5 +1,8 @@
 `timescale 1ns / 1ps
 module tb_max(
 
+
+    //Testing git
+
     );
 endmodule
--- project_1.srcs/sources_1/new/max.sv
+++ project_1.srcs/sources_1/new/max.sv
@@ -3,6 +3,10 @@ module max(
 
 
 //First commnet to check the Git
+
+
+
+//Second comment to check the GIT
         
     );
 endmodule
